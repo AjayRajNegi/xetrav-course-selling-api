40b7362c761da23c956a386a972674999a7b153a
AjayRajNegi <ajayrajnegi111@gmail.com>
2026-02-05 16:03:10 +0530

    Lesson api

--- routes/api/index.ts
+++ routes/api/index.ts
@@ -1,10 +1,12 @@
 import express, { Router } from "express";
 import authRoutes from "./auth.router";
 import courseRoutes from "./courses.router";
+import lessonRoutes from "./lesson.router";
 
 const router: Router = express.Router();
 
 router.use("/auth", authRoutes);
 router.use("/courses", courseRoutes);
+router.use("/lessons", lessonRoutes);
 
 export default router;
